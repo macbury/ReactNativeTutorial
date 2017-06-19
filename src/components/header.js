452c86788b24e1ad7997d2d7f5ec8517c68ecaff
MacBury <macbury@gmail.com>
2017-06-19 20:46:11 +0200

    Make header to be fancy and stuff

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000000',
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  }
+});
 
 export default function Header() {
-  return <Text>Albums!</Text>;
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Albums!</Text>
+    </View>
+  );
 };
